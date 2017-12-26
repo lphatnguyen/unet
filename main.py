75241910a04d9ed3bc9797de592ad5a83ef6477b
Luong Phat NGUYEN <luong.nguyen@insa-cvl.fr>
2017-12-26 15:11:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from torch.utils.data import DataLoader
 from Unet import UNet
 from carvana_dataset import CarvanaDataset
 import numpy as np
+import matplotlib.pyplot as plt
 
 img_csv_file = './data/train_masks.csv'
 train_img_dir = './data/train'
 train_mask_dir = './data/train_masks_png'
 dataset = CarvanaDataset(img_csv_file, train_img_dir, train_mask_dir)
-trainLoader = DataLoader(dataset, shuffle = True, batch_size = 2)
+trainLoader = DataLoader(dataset, shuffle = True, batch_size = 4)
 net = UNet().cuda()
 
 loss_fn = torch.nn.MultiLabelSoftMarginLoss()
-opt = torch.optim.SGD(net.parameters(),lr = 0.00001,momentum=0.5)
+opt = torch.optim.SGD(net.parameters(),lr = 0.000001,momentum=0.5)
 lossValue = []
 opt.zero_grad()
-for epoch in range(25):
+    
+for epoch in range(7):
     runningLoss = 0.0
     for i, datum in enumerate(trainLoader):
         img, label = datum
@@ -28,12 +30,12 @@ for epoch in range(25):
         loss.backward()
         opt.step()
         runningLoss += loss.data[0]
-        if i%200 == 199:
-            print("Loss: ", runningLoss/200)
-            lossValue.append(runningLoss/200)
+        if i%500 == 499:
+            print("Loss: ", runningLoss/500)
+            lossValue.append(runningLoss/500)
             runningLoss = 0
 
-import matplotlib.pyplot as plt
+plt.figure()
 plt.plot(lossValue)
 torch.save(net,'trainedModel.pt')
-torch.save(net.state_dict(),'trained.pt')
+
