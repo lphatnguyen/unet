526753a1d5fa3688a44c47b124426f2ffce8ac95
Luong Phat NGUYEN <luong.nguyen@insa-cvl.fr>
2018-01-01 20:40:32 +0100

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -3,20 +3,32 @@ import PIL.Image as Image
 import torchvision
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import cv2
 
-img = Image.open('./data/test/0bdd2e625f8a_12.jpg')
+path="./data/test/"
+filename=os.listdir(path)
+i=0
 
-img = img.resize((64,64), Image.BILINEAR)
-tensor = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-image = tensor(img)
-net = torch.load('trainedModel.pt')
-inputImg = torch.autograd.Variable(image.unsqueeze(0).cuda())
-outputImg = net(inputImg)
-outputImg = torch.nn.functional.sigmoid(outputImg)
-out = outputImg.squeeze(0).squeeze(0)
-out = out.data.cpu().numpy()
+for string in filename:
+    filename[i]=path+filename[i]
+    img = Image.open(filename[i])
+    
+    img = img.resize((256,256), Image.BILINEAR)
+    tensor = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+    image = tensor(img)
+    net = torch.load('trainedModel2.pt')
+    inputImg = torch.autograd.Variable(image.unsqueeze(0).cuda())
+    outputImg = net(inputImg)
+    outputImg = torch.nn.functional.sigmoid(outputImg)
+    out = outputImg.squeeze(0).squeeze(0)
+    out = out.data.cpu().numpy()
+    out = out.astype(np.uint8)
+    out=out*255
+    i=i+1
+    cv2.imwrite("./output/"+string,out)
 
-out = out.astype(np.uint8)
+
 plt.figure()
 plt.imshow(out)
 plt.figure()
